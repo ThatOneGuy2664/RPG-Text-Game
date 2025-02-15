c42456f66902b3d4dddeaecfe0cf5dd67838edf6
ThatOneGuy2664 <Theboomcaleb2010@gmail.com>
2025-02-15 16:30:13 -0600

    Testing player input handler

--- GameLoop.lua
+++ GameLoop.lua
@@ -1,1 +1,15 @@
-print("Hello")
+function processPlayerInput(input)
+    if input == "look" then
+        print("You see a dark cave entrance ahead.")
+    elseif input == "help" then
+        print("Available commands: look, move, attack, help")
+    elseif input == "move" then
+        print("You move deeper into the forest.")
+    elseif input == "attack" then
+        print("You swing your sword at an imaginary foe.")
+    else
+        print("Unknown command. Type 'help' for a list of commands.")
+    end
+end
+
+_G.processPlayerInput = processPlayerInput -- Expose to JS

8d5f389eb317197aa3b9af00e02b8d95ad394ecd
ThatOneGuy2664 <Theboomcaleb2010@gmail.com>
2025-02-16 12:51:47 -0600

    Console command format

--- GameLoop.lua
+++ GameLoop.lua
@@ -1,7 +1,10 @@
+local lastInputString
 print("GameLoop.lua loaded successfully!")  -- Debug message
 
 function processPlayerInput(input)
     print("Player input received: " .. input)
+    lastInputString = input
+    return ">" .. input
 end
 
 _G.processPlayerInput = processPlayerInput  -- Make function accessible globally

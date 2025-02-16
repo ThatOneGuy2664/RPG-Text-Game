2f88798234f6e8e2b1a85ee77fbfbb16649708d0
ThatOneGuy2664 <Theboomcaleb2010@gmail.com>
2025-02-16 12:58:37 -0600

    Colored text

--- GameLoop.lua
+++ GameLoop.lua
@@ -4,7 +4,7 @@ print("GameLoop.lua loaded successfully!")  -- Debug message
 function processPlayerInput(input)
     print("Player input received: " .. input)
     lastInputString = input
-    return ">" .. input
+    return "> " .. input .. "[BLUE]"
 end
 
 _G.processPlayerInput = processPlayerInput  -- Make function accessible globally

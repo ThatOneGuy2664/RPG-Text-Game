da96cb71a5d90b53da2384305aaa81656cb47031
ThatOneGuy2664 <Theboomcaleb2010@gmail.com>
2025-02-16 14:00:31 -0600

    Game logic test

--- GameLoop.lua
+++ GameLoop.lua
@@ -4,7 +4,17 @@ print("GameLoop.lua loaded successfully!")  -- Debug message
 function processPlayerInput(input)
     print("Player input received: " .. input)
     lastInputString = input
-    return "[blue]> " .. input
+    return input
 end
 
 _G.processPlayerInput = processPlayerInput  -- Make function accessible globally
+
+local js = reqiure "js"
+
+function printToConsole(stringToPrint)
+    js.global.customPrint(stringToPrint)
+end
+
+local function intro()
+    printToConsole([green]Welcome to the game!)
+end
